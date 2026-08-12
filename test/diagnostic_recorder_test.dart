27e90a56794ced37f4702fb714e68ab89937b09d
yamatosaki <66260019+yamatosaki@users.noreply.github.com>
2026-08-13 08:16:56 +0900

    test(诊断): 清理日志测试静态检查

--- test/diagnostic_recorder_test.dart
+++ test/diagnostic_recorder_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:yahagi_kancolle_browser/src/diagnostics/diagnostic_event.dart';
 import 'package:yahagi_kancolle_browser/src/diagnostics/diagnostic_recorder.dart';
