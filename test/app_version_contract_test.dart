ec26e90673b01f835669a3adf353773ebf8e1fbb
yamatosaki <66260019+yamatosaki@users.noreply.github.com>
2026-09-20 18:01:50 +0900

    chore: bump version to 1.0.8-beta.3+17

--- test/app_version_contract_test.dart
+++ test/app_version_contract_test.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('debug and release builds use the 1.0.8-beta.2 version contract', () {
+  test('debug and release builds use the 1.0.8-beta.3 version contract', () {
     final pubspec = File('pubspec.yaml').readAsStringSync();
 
     expect(
       pubspec,
-      contains(RegExp(r'^version: 1\.0\.8-beta\.2\+16$', multiLine: true)),
+      contains(RegExp(r'^version: 1\.0\.8-beta\.3\+17$', multiLine: true)),
     );
   });
 }
